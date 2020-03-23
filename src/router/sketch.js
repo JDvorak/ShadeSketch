8d46e9ea25e8df46b977ba4a854b360e740ede8a
vyvydkf628 <vyvydkf628@naver.com>
2020-03-23 10:47:32 +0900

    add unique id

--- src/router/sketch.js
+++ src/router/sketch.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const multer = require('multer')
 const { spawn } = require('child_process');
-
+const shortId = require('shortid');   
 const router = new express.Router()
 
 const storage = multer.diskStorage({
@@ -9,7 +9,16 @@ const storage = multer.diskStorage({
       cb(null, './val')
     },
     filename: function (req, file, cb) {
-      cb(null,file.originalname) //Appending .jpg
+
+        const uniqueFileName = shortId.generate();
+        cb(null,uniqueFileName+ "_" +file.originalname) //Appending .jpg
+    }
+})
+
+const upload = multer({
+    storage,
+    limits: {
+        fileSize: 1000000
     },
     fileFilter(req, file, cb) {
         if(!file){
@@ -20,36 +29,42 @@ const storage = multer.diskStorage({
         }
         cb(undefined, true)
     }
-})
-
-const upload = multer({
-    storage,
-    limits: {
-        fileSize: 1000000
-    }
     
 })
 
 router.post('/sketch',upload.single('image'),async (req,res)=>{
     const argument = ["./main.py", "--image-size=320", "--direction=810"]
     const pyProg = spawn('python3', argument)
+    console.log(req.file.filename)
     try {
-        if(!req.file) {
-            res.status(400).send("must need be file")
-        }
-        else if(!req.file.originalname.match(/\.(jpg|jpeg|png)$/)){
-            res.status(400).send("must be png jpg jpeg")
-        }
-        else{
-            pyProg.stdout.on('data', function(data) {
-            console.log('runPython func stdout : ' + data.toString());
-            })
+            // pyProg.stdout.on('data', function(data) {
+            // })
             pyProg.on('close', (code) => {
                 console.log('runPython exit code : ' + code);
-                    res.status(200).sendFile(req.file.originalname,{ root: './out' })
+                    res.status(200).sendFile(req.file.filename,{ root: './out' })
                 
             })  
-        }
+    } catch (error) {
+        res.send({error:error.message})
+    }
+},(error,req,res,next)=>{
+    res.status(400).send({error: error.message})
+})
+
+router.post('/sketch/makegif',upload.single('image'),async (req,res)=>{
+    const argument = ["./main_gif.py", "--image-size=320", "--direction=810"]
+    const pyProg = spawn('python3', argument)
+    
+    try {
+        pyProg.stdout.on('data', function(data) {
+        console.log('runPython func stdout : ' + data.toString());
+        })
+        pyProg.on('close', (code) => {
+            console.log('runPython exit code : ' + code);
+                res.status(200).sendFile(req.file.originalname,{ root: './out' })
+            
+        })  
+        
     } catch (error) {
         res.send({error:error.message})
     }
